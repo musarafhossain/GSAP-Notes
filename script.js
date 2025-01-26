dfb2ae038256a9c04f83b8fc588aabdcb6967688
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-01-26 21:53:35 +0530

    Refactor navigation links to use query parameters and improve sidebar functionality

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
         body.classList.toggle('dark');
         toggleMode.classList.toggle('fa-sun');
         toggleMode.classList.toggle('fa-moon');
-        
+
         if (body.classList.contains('dark')) {
             themeStyle.href = 'https://cdn.jsdelivr.net/npm/prismjs/themes/prism-okaidia.css';
         } else {
@@ -21,17 +21,34 @@ document.addEventListener('DOMContentLoaded', () => {
     // Toggle sidebar
     toggleSidebar.addEventListener('click', () => {
         sidebar.classList.toggle('sidebar-open');
-        console.log("first")
     });
 
-    // Load the Markdown file
-    loadMarkdown('1_Introduction to GSAP/README.md');
+    const urlParams = new URLSearchParams(window.location.search);
+    const chapter = urlParams.get('chapter');
 
+    if (chapter) {
+        // Load the Markdown file
+        loadMarkdown(chapter);
+    } else {
+        // Load the Markdown file
+        loadMarkdown('1_Introduction to GSAP');
+    }
 
 
 });
 
+function setChapter(chapter) {
+    const url = new URL(window.location.href);
+
+    // Set the query parameter
+    url.searchParams.set('chapter', chapter);
+
+    // Update the browser's URL without reloading the page
+    window.history.pushState({}, '', url);
+}
+
 async function loadMarkdown(file) {
+    file = `${file}/README.md`
     const response = await fetch(file);
     const markdownText = await response.text();
     const htmlContent = marked.parse(markdownText);
